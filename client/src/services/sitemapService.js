51f9b0bbb6f0555a52c93958fc9ebfeda50049a0
Manas Bajaj <manas.bajaj94@gmail.com>
2018-01-02 09:28:06 +0530

    Fixed error handling.

--- client/src/services/sitemapService.js
+++ client/src/services/sitemapService.js
@@ -4,12 +4,16 @@ export function fetchSitemap(store, url, depth) {
     store.dispatch(fetchSitemapRequest());
     return fetch(`/api/crawl?url=${url}&depth=${depth}`)
         .then(response => {
-            response.json().then(json => {
-                if (!response.ok) {
-                    return store.dispatch(fetchSitemapFailure(json.error));
-                }
+            response.json()
+                .then(json => {
+                    if (!response.ok) {
+                        return store.dispatch(fetchSitemapFailure(json.error));
+                    }
 
-                return store.dispatch(fetchSitemapSuccess(json.data));
-            });
-        }).catch(error => store.dispatch(fetchSitemapFailure(error)));
+                    return store.dispatch(fetchSitemapSuccess(json.data));
+                })
+                .catch(error => {
+                    store.dispatch(fetchSitemapFailure(error.message));
+                });
+        }).catch(error => store.dispatch(fetchSitemapFailure(error.message)));
 }
